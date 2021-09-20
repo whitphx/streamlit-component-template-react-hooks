07c45f222b15ccc5d673b4d2fba27e2eb4d5e23e
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-09-20 11:54:13 +0900

    Fix streamlit-component-lib-react-hooks/.eslintrc.js

--- streamlit-component-lib-react-hooks/.eslintrc.js
+++ streamlit-component-lib-react-hooks/.eslintrc.js
@@ -18,5 +18,10 @@ module.exports = {
       version: "detect",
     },
   },
-  rules: {},
+  rules: {
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md#how-to-use
+    // note you must disable the base rule as it can report incorrect errors
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+  },
 };
