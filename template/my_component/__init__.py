842e112ff5dccf8a7cb48b66296504b899799706
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-03-12 22:32:35 +0900

    Switch from react-scripts to vite and introduce eslint

--- template/my_component/__init__.py
+++ template/my_component/__init__.py
@@ -27,14 +27,14 @@ if not _RELEASE:
         # Pass `url` here to tell Streamlit that the component will be served
         # by the local dev server that you run via `npm run start`.
         # (This is useful while your component is in development.)
-        url="http://localhost:3001",
+        url="http://localhost:5173",
     )
 else:
     # When we're distributing a production version of the component, we'll
     # replace the `url` param with `path`, and point it to to the component's
     # build directory:
     parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "frontend/build")
+    build_dir = os.path.join(parent_dir, "frontend/dist")
     _component_func = components.declare_component("my_component", path=build_dir)
 
 
