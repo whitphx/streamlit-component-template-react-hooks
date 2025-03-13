276bd3f45bc8d6fec62e319c221dabc3678ca37a
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-03-13 17:58:00 +0900

    Fix template to use React 18 properly (#130)

--- template/my_component/frontend/src/index.tsx
+++ template/my_component/frontend/src/index.tsx
@@ -1,13 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { StreamlitProvider } from "streamlit-component-lib-react-hooks"
-import MyComponent from "./MyComponent"
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { StreamlitProvider } from "streamlit-component-lib-react-hooks";
+import MyComponent from "./MyComponent";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <StreamlitProvider>
       <MyComponent />
     </StreamlitProvider>
   </React.StrictMode>,
-  document.getElementById("root")
-)
+);
